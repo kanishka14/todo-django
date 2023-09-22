e92e17b3fa88f5c0e15c6cdd7aaae8cb07d6efb5
Kanishka P <reddykanishka21@gmail.com>
2023-09-23 01:36:29 +0530

    Todo edit and delete functionality created

--- todo_main/views.py
+++ todo_main/views.py
@@ -3,7 +3,9 @@ from todo.models import Task
 
 def home(request):
     tasks = Task.objects.filter(is_compleated=False).order_by('-updated_at') # "-" in orderby will order in decending order
+    compleated_tasks = Task.objects.filter(is_compleated=True)
     context= {
         'tasks':tasks,
+        'compleated_tasks':compleated_tasks,
     }
     return render(request, 'home.html', context)
